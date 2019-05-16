c049aa78e9f097a10e5283e85fd1602af7e2ce48
Rafaela Rodrigues <rafaelaricorodrigues@gmail.com>
2019-05-17 00:18:12 +0100

    new teste

--- Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Controllers/HomeController.cs
+++ Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace Il_Dolce_Chefferini.Controllers
 {
     public class HomeController : Controller
     {
+      
         public IActionResult Index()
         {
             return View("../Confecao/Passo");
---
+++ Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Views/Confecao/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Il_Dolce_Chefferini.Views.Confecao
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
