fcc501ad0e32f6ab13e8d34f541c2679cd012a35
Rafaela Rodrigues <rafaelaricorodrigues@gmail.com>
2019-05-16 22:15:55 +0100

    just discovering how views work

---
+++ Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Controllers/ConfecaoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Il_Dolce_Chefferini.Controllers
+{
+    public class ConfecaoController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace Il_Dolce_Chefferini.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("../Confecao/Passo");
+        }
+    }
+}
