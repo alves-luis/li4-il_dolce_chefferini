71810532c9f73e343e7757b43addd392de8be3cf
Rafaela Rodrigues <rafaelaricorodrigues@gmail.com>
2019-05-17 16:57:29 +0100

    new test

---
+++ Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Controllers/ConfecaoVController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Il_Dolce_Chefferini.Controllers
+{
+    public class ConfecaoVController : Controller 
+    {
+        public ActionResult Index(string button)
+        {
+            return View();
+        }
+    }
+}
