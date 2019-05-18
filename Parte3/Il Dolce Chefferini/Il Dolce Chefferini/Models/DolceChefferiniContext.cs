a09c1325c275d1c01f646fc4b6b648b7a0cb8c5d
Queirós <joaoq98@gmail.com>
2019-05-19 00:30:01 +0100

    BD Context Stuffs
    Still missings things&stuff

--- Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Models/DolceChefferiniContext.cs
+++ Parte3/Il Dolce Chefferini/Il Dolce Chefferini/Models/DolceChefferiniContext.cs
@@ -24,6 +24,8 @@ namespace Il_Dolce_Chefferini.Models
         {
             modelBuilder.Entity<Utilizador>(entity =>
             {
+                entity.HasKey(e => e.id);
+
                 entity.Property(e => e.id)
                     .IsRequired()
                     .HasColumnName("id")
@@ -40,6 +42,9 @@ namespace Il_Dolce_Chefferini.Models
 
                 entity.HasMany(e => e.confecoes)
                     .WithOne();
+
+                entity.HasOne(e => e.ementas)
+                    .WithOne();
             });
 
             modelBuilder.Entity<ConfecaoPasso>(entity =>
@@ -50,33 +55,131 @@ namespace Il_Dolce_Chefferini.Models
                     .IsRequired()
                     .HasColumnName("confecaoId");
 
-                entity.HasOne(e => e.passo);
+                entity.Property(e => e.tempoEmTicks)
+                    .IsRequired()
+                    .HasColumnName("tempoEmTicks");
 
-                entity.HasOne(e => e.confecao);
+                //entity.HasOne(e => e.passo);
+
+                //entity.HasOne(e => e.confecao);
+                
             });
 
             modelBuilder.Entity<Ementa>(entity =>
             {
                 entity.HasKey(e => new {e.almoco, e.utilizadorId, e.diaDaSemana});
+
+                entity.Property(e => e.diaDaSemana)
+                    .IsRequired()
+                    .HasColumnName("diaDaSemana")
+                    .HasMaxLength(64);
+
+                entity.Property(e => e.almoco)
+                    .IsRequired()
+                    .HasColumnName("almoco");
+
             });
 
             modelBuilder.Entity<IngredientePasso>(entity =>
             {
                 entity.HasKey(e => new {e.ingredienteId, e.receitaId, e.numeroSequenciaPasso});
+
+                entity.Property(e => e.quantidade)
+                    .IsRequired()
+                    .HasColumnName("quantidade");
+
+                entity.Property(e => e.unidade)
+                    .IsRequired()
+                    .HasColumnName("unidade")
+                    .HasMaxLength(32);
             });
 
             modelBuilder.Entity<Passo>(entity =>
             {
                 entity.HasKey(e => new {e.receitaId, e.numeroSequencia});
+
+                entity.Property(e => e.numeroSequencia)
+                    .IsRequired()
+                    .HasColumnName("numeroSequencia")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.descricao)
+                    .IsRequired()
+                    .HasColumnName("descricao")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.tempoEsperadoEmTicks)
+                    .IsRequired()
+                    .HasColumnName("tempoEsperadoEmTicks");
+
+                entity.Property(e => e.aspetoEsperado)
+                    .IsRequired()
+                    .HasColumnName("aspetoEsperado")
+                    .HasMaxLength(128);
+
+                entity.Property(e => e.urlVideo)
+                    .HasColumnName("urlVideo")
+                    .HasMaxLength(512);
+
+               /* entity.HasMany(e => e.ingredientesPassos)
+                     .WithOne(); */
             });
 
             modelBuilder.Entity<Receita>(entity =>
             {
                 entity.HasKey(e => e.id);
+
                 entity.Property(e => e.id)
                     .IsRequired()
                     .HasColumnName("id")
                     .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.nome)
+                    .IsRequired()
+                    .HasColumnName("nome")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.descricao)
+                    .IsRequired()
+                    .HasColumnName("descricao")
+                    .HasMaxLength(511);
+
+                entity.Property(e => e.tempoEsperadoEmTicks)
+                    .IsRequired()
+                    .HasColumnName("tempoEsperadoEmTicks");
+
+                entity.Property(e => e.grauDificuldade)
+                    .IsRequired()
+                    .HasColumnName("grauDificuldade");
+
+                entity.Property(e => e.doses)
+                    .IsRequired()
+                    .HasColumnName("doses");
+
+                entity.Property(e => e.calorias)
+                    .IsRequired()
+                    .HasColumnName("calorias");
+
+                entity.Property(e => e.lipidos)
+                    .IsRequired()
+                    .HasColumnName("lipidos");
+
+                entity.Property(e => e.hidratos)
+                    .IsRequired()
+                    .HasColumnName("hidratos");
+
+                entity.Property(e => e.proteinas)
+                    .IsRequired()
+                    .HasColumnName("proteinas");
+
+                entity.Property(e => e.criador)
+                    .IsRequired()
+                    .HasColumnName("criador")
+                    .HasMaxLength(64);
+
+               /* entity.HasMany(e => e.passo)
+                    .WithOne(); */
+
             });
             
             modelBuilder.Entity<Confecao>(entity =>
@@ -86,17 +189,77 @@ namespace Il_Dolce_Chefferini.Models
                     .IsRequired()
                     .HasColumnName("id")
                     .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.usouAjuda)
+                    .IsRequired()
+                    .HasColumnName("usouAjuda");
+
+                entity.Property(e => e.bemSucedida)
+                    .IsRequired()
+                    .HasColumnName("bemSucedida");
             });
             
             modelBuilder.Entity<Avaliacao>(entity =>
             {
                 entity.HasKey(e => e.confecaoId);
                 entity.HasOne(e => e.confecao);
+
+                entity.Property(e => e.dificuldade)
+                    .IsRequired()
+                    .HasColumnName("dificuldade");
+
+                entity.Property(e => e.utilidadeAjudas)
+                    .HasColumnName("utilidadeAjudas");
+
+                entity.Property(e => e.grauSatisfacao)
+                    .IsRequired()
+                    .HasColumnName("grauSatisfacao");
+
+                entity.Property(e => e.data)
+                    .IsRequired()
+                    .HasColumnName("data");
             });
             
             modelBuilder.Entity<Temperatura>(entity =>
             {
                 entity.HasKey(e => e.id);
+
+                entity.Property(e => e.id)
+                    .IsRequired()
+                    .HasColumnName("id")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.nome)
+                    .IsRequired()
+                    .HasColumn("nome")
+                    .HasMaxLength(64);
+            });
+
+            modelBuilder.Entity<Ingrediente>(entity =>
+            {
+                entity.HasKey(e => e.id);
+
+                entity.Property(e => e.id)
+                    .IsRequired()
+                    .HasColumnName("id")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.nome)
+                    .IsRequired()
+                    .HasColumnName("nome")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.imagem)
+                    .HasColumnName("image")
+                    .HasMaxLength(128);
+
+                entity.Property(e => e.comida)
+                    .IsRequired()
+                    .HasColumnName("comida");
+
+               /* entity.HasMany(e => e.ingredientesPassos)
+                    .WithOne(); */
+
             });
         }
     }
